e9bf44ecdbd0ab8b6bc689e97a4f28d7a23d2d23
MosesOmaej <78677317+mosesomaej@users.noreply.github.com>
2023-04-01 23:02:55 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "inst" {
+  count           = var.instance_count
+  ami             = var.ami
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet
+
+  tags  = {
+    Name = "Mozees-${count.index}"
+  }
+  
+}
+
+variable "ami" {}
+
+variable "instance_count" {}
+
+variable "instance_type" {}
+
+variable "subnet" {}
+
+output "aws_instances"{
+  value = aws_instance.inst.*.tags.Name
+  }
